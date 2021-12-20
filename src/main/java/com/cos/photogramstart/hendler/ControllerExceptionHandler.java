a08e77228b1141c26cebbdffe5fd852dc421aaf6
unknown <fj2008@naver.com>
2021-12-20 10:38:53 +0900

    이미지엄로드 유효성체크 완료

--- src/main/java/com/cos/photogramstart/hendler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/hendler/ControllerExceptionHandler.java
@@ -22,7 +22,13 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(CustomValidationException.class)
 	public String validationException(CustomValidationException e) {
 		
-		return Script.back(e.getErrorMap().toString());
+		if(e.getErrorMap() == null) {
+			return Script.back(e.getMessage());
+		}else {
+			return Script.back(e.getErrorMap().toString());
+		}
+		
+		
 	}
 	
 	
